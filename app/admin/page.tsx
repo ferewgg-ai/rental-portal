f7c19bce749d4d352955387e3cec5b5e13c5a534
Ferew Haile <ferewgg@gmail.com>
2025-09-18 03:43:06 -0400

    Build fixes: add ESLint, valid Admin page, ignore lint during build, force UTF-8

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,3 @@
-export default function AdminPage() {
-  return (
-    <h1>Admin Page</h1>
-  );
-}
-
+﻿export default function AdminPage() {
+  return <h1 className='p-6 text-2xl font-semibold'>Admin Page</h1>;
 }
